7f8d24cd385a99f0d84cdf004effdfb33d6c7c09
Shashank <shashank.keshava@bookmyshow.com>
2018-04-26 19:00:47 +0530

    Integrated Redux devetools plugin

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './container/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-import {createLogger} from "redux-logger";
-import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 
-import Reducer from "./reducers";
-import initialState from './data'
+import reducer from './reducers';
+import initialState from './data';
 
-const logger = createLogger({timestamp: true})
+const logger = createLogger({ timestamp: true });
 
-let store = createStore(Reducer, initialState, applyMiddleware(thunk, logger),
-/* preloadedState, */
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+let store = createStore(
+    reducer,
+    initialState,
+    compose(
+        applyMiddleware(thunk, logger),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ),
+    /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
     <Provider store={store}>
-    <App/>
-</Provider>, document.getElementById('root'));
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
