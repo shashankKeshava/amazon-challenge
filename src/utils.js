881f3110ada4f91e3360aff590553a05f36bfa5c
Shashank <keshavashashank@gmail.com>
2018-04-28 12:36:25 +0530

    Added filters in utils

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,23 @@
-module.exports={
-    loadingMsg:{
-        content:"Loading......",
-        pluginLink:"https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=en",
-        importantMsg:"Install Chrome Allow-Control-Allow-Origin: * Plugin and Enable it"
+module.exports = {
+    loadingMsg: {
+        content: "Loading......",
+        pluginLink: "https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeac" +
+                "fghkpbjhddihlkkiljbi?hl=en",
+        importantMsg: "Install Chrome Allow-Control-Allow-Origin: * Plugin and Enable it"
+    },
+    filters: {
+        search: {
+            placeholder: 'Search'
+        },
+        groupBy: {
+            placeholder: 'Group By'
+        },
+        orderBy: {
+            placeholder: 'Order By'
+        },
+        ratingBy: {
+            label: 'Filter By:',
+            rates: [1, 2, 3, 4, 5]
+        }
     }
 }
